b5a5c20b543ca8947829ceadf96616161351a941
vinjar <vinjar.dev@gmail.com>
2025-12-19 18:46:26 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demomesser
+
+Hjemmeside for demomesser.no
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+
+  return (
+    <>
+        <h2 className="text-2xl text-blue-500">Hello</h2>     
+    </>
+  )
+}
