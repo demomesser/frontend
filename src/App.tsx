fd1d5b3350a1d612983e7da6b2fa092e9b09320e
vinjar <vinjar.dev@gmail.com>
2026-01-16 15:09:43 +0100

    added page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage"
+import NotFoundPage from "./pages/PageNotFound";
 
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
         </Routes>
     )
 }
