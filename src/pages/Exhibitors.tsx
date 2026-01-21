d07a265e05e86d53afd6fd9720d245128fd90189
vinjar <vinjar.dev@gmail.com>
2026-01-21 21:15:56 +0100

    added two more exhibitors

--- src/pages/Exhibitors.tsx
+++ src/pages/Exhibitors.tsx
@@ -10,15 +10,32 @@ import { useState } from "react"
 
 
 import SeweLogo from "../assets/SeweMaskin_logo.svg";
+import WeidemannLogo from "../assets/weidemann_logo.png"
+import KonwiLogo from "../assets/konwi_logo.svg"
 
 const exhibitors: Exhibitor[] = [
     {
         id: 1,
-        name: "test",
+        name: "sewe maskin",
         description: "Norcar leverer minilastere med løftekapasitet fra 750 kg til over 1500 kg og motoreffekter fra 20 hk til 49,6 hk. Maskinene kan leveres i ulike konfigurasjoner og med en rekke tilleggsutstyr, alt etter dine behov.",
         image: SeweLogo,
         link: "https://www.sewemaskin.no/"
     },
+    {
+        id: 2,
+        name: "Weidemann",
+        description: "Weidemann tilbyr kompakte Hoftracs, hjullastere, teleskopiske hjullastere, teleskoplastere og et bredt utvalg av redskaper og tilbehør.",
+        image: WeidemannLogo,
+        link: "https://www.weidemann.com/uk/"
+    },
+    {
+        id: 3,
+        name: "Konwi",
+        description: "KONWI AS har siden 1981 opparbeidet seg 30 års lang erfaring med fremstilling av nye produkter og salg av vel innarbeidede kirkegårdsprodukter til kommuner, steinhuggere, gartnere og privatpersoner.",
+        image: KonwiLogo,
+        link: "https://www.kirketorget.as/"
+    },
+    
 ]
 
 const NAV_ITEMS: NavItem[] = [
